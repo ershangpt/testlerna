f9b4511ed50562e02819278ef8525e7b369d90b5
Shashank <ershangpt@gmail.com>
2019-01-26 23:48:31 +0530

    update circleci

--- packages/lernang/src/app/app.component.spec.ts
+++ packages/lernang/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'lernang'`, () => {
+  it(`should have as title 'My lernang'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('lernang');
+    expect(app.title).toEqual('My lernang');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to lernang!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to My lernang!');
   });
 });
